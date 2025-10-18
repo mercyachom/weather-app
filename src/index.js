1414d33106f17575391c8d3245d0dc6534c69e0f
mercyachom <achommercy137@gmail.com>
2025-10-18 14:27:34 +0300

    added dominant city

--- src/index.js
+++ src/index.js
@@ -24,3 +24,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
