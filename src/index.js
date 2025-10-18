5021595ddf93fb8405737c37653163c2018903e9
mercyachom <achommercy137@gmail.com>
2025-10-18 14:00:20 +0300

    added axios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+function searchCity(city) {
+  let apiKey() = "4bcdoaaff781057betcf5af877a53545";
+  let apiUrl() = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${metric}`;
+
+  axios.get(apiUrl).then(refreshweather);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
